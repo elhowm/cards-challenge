f6d037674192dec354867714594dfe5cb1c3d0fc
Kris <elhowm@gmail.com>
2016-11-10 15:45:10 +0200

    Base checker

--- lib/checkers/base.rb
+++ lib/checkers/base.rb
@@ -1,9 +1,15 @@
 module Checkers
   class Base
-    attr_reader :cards
+    attr_reader :cards, :kicker_cards
 
     def initialize(cards)
       @cards = cards
+      @kicker_cards = []
+    end
+
+    def score
+      combinations = Judge::COMBINATIONS
+      combinations.size - combinations.index(self::class.name)
     end
   end
 end
---
+++ spec/lib/checkers/base_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+RSpec.describe Checkers::Base do
+  include CardsFactory
+
+  let(:cards) { [] }
+  let(:checker) { described_class.new(cards) }
+
+  describe '#score' do
+    context 'when class name is Flush' do
+      before do
+        expect(described_class).to receive(:name).and_return('Flush')
+      end
+
+      it { expect(checker.score).to eq(6) }
+    end
+
+    context 'when class name is Pair' do
+      before do
+        expect(described_class).to receive(:name).and_return('OnePair')
+      end
+
+      it { expect(checker.score).to eq(2) }
+    end
+  end
+end

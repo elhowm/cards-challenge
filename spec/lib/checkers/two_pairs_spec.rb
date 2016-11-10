21e5ec05f76e6083bc8e8ab99678081bd19c52ed
Kris <elhowm@gmail.com>
2016-11-10 14:22:42 +0200

    Two pairs

---
+++ spec/lib/checkers/two_pairs_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+RSpec.describe Checkers::TwoPairs do
+  include CardsFactory
+
+  let(:cards) { [] }
+  let(:checker) { described_class.new(cards) }
+
+  describe '#bingo?' do
+    context 'when there are two pairs' do
+      let(:cards) { make_cards(*%w(2D 2H TC TS AD)) }
+
+      it { expect(checker.send(:bingo?)).to eq(true) }
+    end
+
+    context 'when there aren\'t two pairs' do
+      context '(example 1)' do
+        let(:cards) { make_cards(*%w(2D 2H TC TS TD)) }
+
+        it { expect(checker.send(:bingo?)).to eq(false) }
+      end
+
+      context '(example 2)' do
+        let(:cards) { make_cards(*%w(2D QD 3C TS 9D)) }
+
+        it { expect(checker.send(:bingo?)).to eq(false) }
+      end
+    end
+  end
+end

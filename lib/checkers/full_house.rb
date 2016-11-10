99167a295e0119532f9b68d1faedca3a6b3a7f5f
Kris <elhowm@gmail.com>
2016-11-10 14:33:21 +0200

    Refactor Full house

--- lib/checkers/full_house.rb
+++ lib/checkers/full_house.rb
@@ -1,19 +1,9 @@
 module Checkers
   class FullHouse < Base
     def bingo?
-      three_of_a_kind? && one_pair?
-    end
-
-    private
-
-    def three_of_a_kind?
-      faces = cards.map(&:face)
-      faces.any? { |face| faces.count(face) == 3 }
-    end
-
-    def one_pair?
-      faces = cards.map(&:face)
-      faces.any? { |face| faces.count(face) == 2 }
+      pair = Checkers::Pair.new(cards).bingo?
+      three = Checkers::ThreeOfAKind.new(cards).bingo?
+      pair && three
     end
   end
 end
--- spec/lib/checkers/full_house_spec.rb
+++ spec/lib/checkers/full_house_spec.rb
@@ -7,66 +7,25 @@ RSpec.describe Checkers::FullHouse do
   let(:checker) { described_class.new(cards) }
 
   describe '#bingo?' do
-    context 'when condition becomes true' do
+    let(:pair) { Checkers::Pair }
+    let(:three) { Checkers::ThreeOfAKind }
+
+    context 'when there is full house' do
       before do
-        expect(checker).to receive(:three_of_a_kind?).and_return(true)
-        expect(checker).to receive(:one_pair?).and_return(true)
+        expect_any_instance_of(three).to receive(:bingo?).and_return(true)
+        expect_any_instance_of(pair).to receive(:bingo?).and_return(true)
       end
 
       it { expect(checker.bingo?).to eq(true) }
     end
 
-    context 'when condition becomes false' do
+    context 'when there isn\'t full house' do
       before do
-        expect(checker).to receive(:three_of_a_kind?).and_return(true)
-        expect(checker).to receive(:one_pair?).and_return(false)
+        expect_any_instance_of(three).to receive(:bingo?).and_return(true)
+        expect_any_instance_of(pair).to receive(:bingo?).and_return(false)
       end
 
       it { expect(checker.bingo?).to eq(false) }
-    end
-  end
-
-  describe '#three_of_a_kind?' do
-    context 'when there are three eq faces' do
-      let(:cards) { make_cards(*%w(2D KD TC TS TD)) }
-
-      it { expect(checker.send(:three_of_a_kind?)).to eq(true) }
-    end
-
-    context 'when there aren\'t three eq faces' do
-      context '(example 1)' do
-        let(:cards) { make_cards(*%w(2D TD TC TS TD)) }
-
-        it { expect(checker.send(:three_of_a_kind?)).to eq(false) }
-      end
-
-      context '(example 2)' do
-        let(:cards) { make_cards(*%w(2D QD 3C TS TD)) }
-
-        it { expect(checker.send(:three_of_a_kind?)).to eq(false) }
-      end
-    end
-  end
-
-  describe '#one_pair?' do
-    context 'when there is one pair' do
-      let(:cards) { make_cards(*%w(TD TH QD QS QH)) }
-
-      it { expect(checker.send(:one_pair?)).to eq(true) }
-    end
-
-    context 'when there isn\'t one pair' do
-      context '(example 1)' do
-        let(:cards) { make_cards(*%w(TD JH QD KS 2H)) }
-
-        it { expect(checker.send(:one_pair?)).to eq(false) }
-      end
-
-      context '(example 2)' do
-        let(:cards) { make_cards(*%w(TD TH TC KS 2H)) }
-
-        it { expect(checker.send(:one_pair?)).to eq(false) }
-      end
     end
   end
 end

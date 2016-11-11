19ee3f752485bbfdfe797d4ed52888e9f65e2274
Kris <elhowm@gmail.com>
2016-11-11 03:14:36 +0200

    add examples for four of a kind

--- spec/lib/checkers/four_of_a_kind_spec.rb
+++ spec/lib/checkers/four_of_a_kind_spec.rb
@@ -7,13 +7,13 @@ RSpec.describe Checkers::FourOfAKind do
 
   describe '#bingo?' do
     context 'when there are four eq faces' do
-      let(:cards) { make_cards(*%w(2D AH AD AS AC)) }
+      let(:cards) { make_cards(*%w(2D AH AD AS AC 7D 8S)) }
 
       it { expect(checker.bingo?).to eq(true) }
     end
 
     context 'when there aren\'t four eq faces' do
-      let(:cards) { make_cards(*%w(2D AH AD TS AC)) }
+      let(:cards) { make_cards(*%w(2D AH AD TS AC 7D 8S)) }
 
       it { expect(checker.bingo?).to eq(false) }
     end

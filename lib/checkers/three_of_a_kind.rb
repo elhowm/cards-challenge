79c8ea34924e06c75ab5046f5eb66887dae7812a
Kris <elhowm@gmail.com>
2016-11-11 03:13:08 +0200

    add more examples for 3 of a kind

--- lib/checkers/three_of_a_kind.rb
+++ lib/checkers/three_of_a_kind.rb
@@ -5,7 +5,7 @@ module Checkers
     def bingo?
       faces = cards.map(&:face)
       @kicker_cards = cards.select { |card| faces.count(card.face) == 3 }
-      !@kicker_cards.empty?
+      !kicker_cards.empty?
     end
   end
 end
--- spec/lib/checkers/three_of_a_kind_spec.rb
+++ spec/lib/checkers/three_of_a_kind_spec.rb
@@ -8,20 +8,20 @@ RSpec.describe Checkers::ThreeOfAKind do
 
   describe '#bingo?' do
     context 'when there is three of a kind' do
-      let(:cards) { make_cards(*%w(2D KD TC TS TD)) }
+      let(:cards) { make_cards(*%w(2D KD TC TS TD 4C 5C)) }
 
       it { expect(checker.send(:bingo?)).to eq(true) }
     end
 
     context 'when there isn\'t three of a kind' do
       context '(example 1)' do
-        let(:cards) { make_cards(*%w(2D TD TC TS TD)) }
+        let(:cards) { make_cards(*%w(2D TD TC TS TD 4C AC)) }
 
         it { expect(checker.send(:bingo?)).to eq(false) }
       end
 
       context '(example 2)' do
-        let(:cards) { make_cards(*%w(2D QD 3C TS TD)) }
+        let(:cards) { make_cards(*%w(2D QD 3C TS TD 4C AC)) }
 
         it { expect(checker.send(:bingo?)).to eq(false) }
       end

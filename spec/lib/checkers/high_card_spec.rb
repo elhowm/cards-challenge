2bd5e24b5b27948fc72bf966cf8daac71b9df3e1
Kris <elhowm@gmail.com>
2016-11-11 03:16:09 +0200

    fix name high_card_spec

--- spec/lib/checkers/high_card_spec.rb
+++ spec/lib/checkers/high_card_spec.rb
@@ -8,13 +8,13 @@ RSpec.describe Checkers::HighCard do
 
   describe '#bingo?' do
     context 'example 1' do
-      let(:cards) { make_cards(*%w(TS TC AH AD)) }
+      let(:cards) { make_cards(*%w(TS TC AH AD 7D 7C 8S)) }
 
       it { expect(checker.bingo?).to eq(true) }
     end
 
     context 'example 2' do
-      let(:cards) { make_cards(*%w(TS JC KH AD)) }
+      let(:cards) { make_cards(*%w(TS JC KH AD 7D 4D 8S)) }
 
       it { expect(checker.bingo?).to eq(true) }
     end
